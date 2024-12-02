87da0c9fa3cb1674a38688fd02bc0045e2fc2ab9
brijesh-vadi <brijeshvadi@proton.me>
2024-12-02 18:53:40 +0530

    fix: build issue

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import businessProfileRoute from './routes/businessProfileRoute.js';
 import businessCategoryRoute from './routes/businessCategoryRoute.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ if (process.env.MODE === 'DEVELOPMENT') {
 }
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, Railway is working!');

bb15f71c6f8c9db82a55f31a1be0ea1c6ea2889c
brijesh-vadi <brijeshvadi@proton.me>
2024-12-02 17:38:24 +0530

    fix:port issue

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ dotenv.config({
   path: './.env',
 });
 
-const port = parseInt(process.env.PORT || '8080', 10);
+const port = parseInt(process.env.PORT || '3000', 10);
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`App running on http://0.0.0.0:${port}`);
